e936f893e4d639301c502ce309586100228019d9
lufan <lufan@163.com>
2017-06-21 22:29:03 +0800

    完成添加和编辑讲师功能

--- mybxg/public/js/teacher-add.js
+++ mybxg/public/js/teacher-add.js
@@ -2,19 +2,25 @@ define(['jquery','util','template'],function($,util,template){
 	// 导航选中
 	util.setMenu('/teacher/list');
 	// 提交表单处理
-	$('#addTeacherBtn').click(function(){
+	function submitForm(url){
+		$('#addTeacherBtn').click(function(){
 		// console.log(0);
 		// alert(1);
 		$.ajax({
 			type:'post',
-			url:'/api/teacher/add',
+			url:url,
 			data:$('#addTeacherForm').serialize(),
 			dataType:'json',
 			success:function(data){
-				console.log(data);
+				// console.log(data);
+				if(data.code==200){
+					location.href='/teacher/list';
+				}
 			}
+			});
 		});
-	});
+	}
+	
 	// 获取参数中tc_id
 	var tcId=util.qs('tc_id',location.search);
 	if(tcId){
@@ -31,14 +37,22 @@ define(['jquery','util','template'],function($,util,template){
 				data.result.operateFlag='编辑';
 				var html=template('teacherFormTpl',data.result);
 				$('#teacherFormInfo').html(html);
+			
+					// 编辑提交
+					submitForm('/api/teacher/update');
+			
+					
+			
 			}
 		});
 
 	}else{
 		// 添加讲师操作-----填充一个空的表单
-		var html=template('teacherFormTpl',{operateFlag:'添加'});
+		var html=template('teacherFormTpl',{operateFlag:'添加',tc_gender:1});
 		$('#teacherFormInfo').html(html);
 		$('#navFlag').html('讲师添加');
+		// 添加提交
+		submitForm('/api/teacher/add');
 
 	}
 });
